b80f86ca0398685c1e10e14af9598c16247b02c5
Rafa Perez <rapeop1c@gmail.com>
2023-07-31 19:44:06 +0200

    New Screens

---
+++ 07-WidgetsApp/widgets_app/lib/presentation/screen/infinite_scroll/infinite_scroll_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class InfiniteScrollScreen extends StatelessWidget {
+  
+  static const String name = 'infinite_screen';
+  
+  const InfiniteScrollScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ 07-WidgetsApp/widgets_app/lib/presentation/screen/ui_controls/ui_controls_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class UiControlsScreen extends StatelessWidget {
+  
+  static const String name = 'ui_screen';
+  
+  const UiControlsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
